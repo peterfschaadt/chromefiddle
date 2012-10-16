d8c9444d78887d9cab46ff5c4dcea63fd569abca
Peter Schaadt <peterfschaadt@yahoo.com>
2012-10-16 00:47:41 -0400

    Added unicode method.

--- flags/models.py
+++ flags/models.py
@@ -16,3 +16,6 @@ class Flag(models.Model):
 	date_added = models.DateTimeField(auto_now_add=True)
 	date_modified = models.DateTimeField(auto_now=True)
 
+	# Print name of flag
+	def __unicode__(self):
+		return self.name
