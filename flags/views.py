9c9e9f60ec0c9bde697eb47bf1d89d8576e7ea4e
Peter Schaadt <peterfschaadt@yahoo.com>
2013-02-12 02:26:58 -0800

    Added home page view.

--- flags/views.py
+++ flags/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('This is the Google Chrome experimental flag index from chrome://flags')
+    return render(request, 'home.html', {})
