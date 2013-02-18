552900b3fee6e1f8802338a67e2ca1688ccd7b76
Peter Schaadt <peterfschaadt@yahoo.com>
2013-02-17 19:10:49 -0800

    Added log statements for determining user's browser info.

--- flags/views.py
+++ flags/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-# import httpagentparser
+import httpagentparser
+from django.utils.log import getLogger
+logger = getLogger('app')
 
 def home(request):
-    # user_agent = request.META['HTTP USER AGENT']
+    # Pull user agent string from request
+    user_agent = request.META['HTTP USER AGENT']
+    logger.warning('User agent string: ' + user_agent)
     # Parse user agent string for OS, browser, and version
-    # user_info = httpagentparser.detect(user_agent)
+    user_info = httpagentparser.detect(user_agent)
+    logger.warning('User info: ' + user_info)
     return render(request, 'home.html', {})
