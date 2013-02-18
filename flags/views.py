7673f3c15a64d685f41c5bbab89cae0178c53fe7
Peter Schaadt <peterfschaadt@yahoo.com>
2013-02-17 23:02:57 -0800

    Added user info detection and test user string.

--- flags/views.py
+++ flags/views.py
@@ -12,4 +12,22 @@ def home(request):
     # Parse user agent string for OS, browser, and version
     user_info = httpagentparser.detect(user_agent)
     logger.info('USER INFO: %s' % user_info)
+    # test_user_info = {'flavor': {'version': 'X 10.8.2', 'name': 'MacOS'}, 'os': {'name': 'Macintosh'}, 'browser': {'version': '24.0.1312.57', 'name': 'Chrome'}}
+
+    # Check for operating system type
+    operating_system = user_info['os']['name'].lowercase
+    if operating_system == 'macintosh':
+        os = 'Mac OS X'
+    elif operating_system == 'windows':
+        os = 'Windows'
+    elif operating_system == 'linux':
+        os = 'Linux'
+    elif operating_system == 'chrome-os':
+        os = 'Chrome OS'
+    elif operating_system == 'android':
+        os = 'Android'
+
+    # Build object with OS Name, Browser Name, and Browser Version
+    user_stats = {'os': os, 'browser': user_info['browser']}
+
     return render(request, 'home.html', {})
