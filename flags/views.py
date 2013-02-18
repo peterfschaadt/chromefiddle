479ccaab6f647eedf958c3f578adc62c7c7c272c
Peter Schaadt <peterfschaadt@yahoo.com>
2013-02-17 18:50:12 -0800

    Adding user agent detection.

--- flags/views.py
+++ flags/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+# import httpagentparser
 
 def home(request):
+    # user_agent = request.META['HTTP USER AGENT']
+    # Parse user agent string for OS, browser, and version
+    # user_info = httpagentparser.detect(user_agent)
     return render(request, 'home.html', {})
