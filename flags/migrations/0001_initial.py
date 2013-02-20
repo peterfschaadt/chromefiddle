3175277506a9d9fd85b900c5375aa23ce071ed87
Peter Schaadt <peterfschaadt@yahoo.com>
2013-02-20 00:16:33 -0800

    Added migrations to directory for South.

---
+++ flags/migrations/0001_initial.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Flag'
+        db.create_table('flags_flag', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('description', self.gf('django.db.models.fields.CharField')(max_length=300)),
+            ('compatibility', self.gf('django.db.models.fields.CharField')(max_length=10)),
+            ('date_added', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('date_modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal('flags', ['Flag'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Flag'
+        db.delete_table('flags_flag')
+
+
+    models = {
+        'flags.flag': {
+            'Meta': {'object_name': 'Flag'},
+            'compatibility': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '300'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['flags']
