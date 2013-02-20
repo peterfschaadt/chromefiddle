fdca274c38a3053ebf211ef5b09c5c434ebc360f
Peter Schaadt <peterfschaadt@yahoo.com>
2013-02-20 00:17:27 -0800

    Admin section with documentation enabled.

--- chromefiddle/urls.py
+++ chromefiddle/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # url(r'^chromefiddle/', include('chromefiddle.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # App-specific URLs
     # url(r'^$', 'flags.views.home'),
---
+++ flags/admin.py
@@ -0,0 +1,4 @@
+from flags.models import Flag
+from django.contrib import admin
+
+admin.site.register(Flag)
