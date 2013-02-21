68dc4ebc04ec4da38195a250add20074d1ae5c79
Peter Schaadt <peterfschaadt@yahoo.com>
2013-02-21 00:44:51 -0800

    Simplified URL routing to one file.

--- chromefiddle/urls.py
+++ chromefiddle/urls.py
@@ -8,25 +8,25 @@ from django.contrib import admin
 admin.autodiscover()
 
 # Import URLs from apps
-import flags.urls
+# import flags.urls
 
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'chromefiddle.views.home', name='home'),
-    # url(r'^chromefiddle/', include('chromefiddle.foo.urls')),
+    # url(r'^flags/', include('chromefiddle.flags.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
+    # Admin documentation enabled
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # App-specific URLs
-    # url(r'^$', 'flags.views.home'),
-    url(r'^$', include(flags.urls)),
-    # Original:
-    # url(r'^flags/', include(flags.urls)),
-
     # Admin enabled
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin-only/', include(admin.site.urls)),
 
-    # url(r'.*', redirect_to, {'url': '/flags/home'}),
+    # App-specific URLs
+    url(r'^$', 'flags.views.home'),
+    url(r'^home', 'flags.views.home'),
+    url(r'^about', 'flags.views.about'),
+    url(r'^list', 'flags.views.list'),
+
+    # url(r'.*', redirect_to, {'url': '/home'}),
 )
--- flags/admin.py
+++ flags/admin.py
@@ -1,4 +1,5 @@
 from flags.models import Flag
 from django.contrib import admin
 
+# Include Flags in Django admin
 admin.site.register(Flag)
