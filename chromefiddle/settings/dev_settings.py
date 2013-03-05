69ea640c519d91041514147b90714f775e503475
Peter Schaadt <peterfschaadt@yahoo.com>
2013-03-04 19:19:51 -0800

    Updated settings with media URLs.

--- chromefiddle/settings/dev_settings.py
+++ chromefiddle/settings/dev_settings.py
@@ -38,10 +38,14 @@ EMAIL_HOST_PASSWORD = DEV_EMAIL_PASSWORD
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = '/Users/peter/code/python/django/chromefiddle/media/'
+
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
