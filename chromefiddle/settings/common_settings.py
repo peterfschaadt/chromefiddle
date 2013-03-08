63b3437dd759eb52a93dae01dac36355efe08b90
Peter Schaadt <peterfschaadt@yahoo.com>
2013-03-07 18:57:33 -0800

    First commit for adding comments to flag details pages.

--- chromefiddle/settings/common_settings.py
+++ chromefiddle/settings/common_settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.comments',
     'gunicorn',
     'flags',
     'south',
