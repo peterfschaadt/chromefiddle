58989fec4ddfd55e009f7b6073d691d1fc2a1489
Peter Schaadt <peterfschaadt@yahoo.com>
2013-03-25 19:30:20 -0700

    Added django_dumpdb to install apps.

--- chromefiddle/settings/common_settings.py
+++ chromefiddle/settings/common_settings.py
@@ -85,6 +85,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'django_dumpdb',
 )
 
 # Google Analytics asynchronous tracking
