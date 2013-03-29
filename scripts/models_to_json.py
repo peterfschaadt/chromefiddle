cf59acda979dc716bd1101ce120da9d38921f30d
Peter Schaadt <peterfschaadt@yahoo.com>
2013-03-29 15:38:36 -0700

    Fixed models_to_json dump script.

--- scripts/models_to_json.py
+++ scripts/models_to_json.py
@@ -6,7 +6,7 @@ Serialize all Flags to JSON file.
 """
 
 # Retrieve and serialize all Flags from database
-data = serializers.serialize('json', modes.Flag.objects.all())
+data = serializers.serialize('json', models.Flag.objects.all())
 # Create JSON file to store Flags
 output = open('flags.json', 'w')
 # Write and Close
